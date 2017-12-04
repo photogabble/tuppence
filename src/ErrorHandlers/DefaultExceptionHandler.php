c0aa3ff2997dea91d7c0a2cbdd56266325587d89
Simon Dann <carbontwelve@users.noreply.github.com>
2017-12-04 14:13:26 +0000

    :sparkles: added ability to ignore exceptions

--- src/ErrorHandlers/DefaultExceptionHandler.php
+++ src/ErrorHandlers/DefaultExceptionHandler.php
@@ -5,16 +5,31 @@ namespace Photogabble\Tuppence\ErrorHandlers;
 use Exception;
 use League\Route\Http\Exception\NotFoundException as RouteNotFoundException;
 use League\Container\Exception\NotFoundException as ContainerNotFoundException;
+use Psr\Http\Message\RequestInterface;
 use Zend\Diactoros\Response\JsonResponse;
 
 class DefaultExceptionHandler implements ExceptionHandler
 {
+
+    /**
+     * Exceptions this handler should ignore and pass through.
+     *
+     * @var array
+     */
+    protected $ignore = [];
+
     /**
      * @param Exception|RouteNotFoundException|ContainerNotFoundException $e
+     * @param RequestInterface $request
      * @return \Psr\Http\Message\ResponseInterface
+     * @throws Exception
      */
-    public function __invoke(Exception $e)
+    public function __invoke(Exception $e, RequestInterface $request)
     {
+        if (in_array(get_class($e), $this->ignore)) {
+            throw $e;
+        }
+
         return new JsonResponse([
             'message' => $e->getMessage(),
             'trace' => explode(PHP_EOL, $e->getTraceAsString())
