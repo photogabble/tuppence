f4aeb1bebfb27e8d9f654f2674b7589c0e604b12
Simon <carbontwelve@users.noreply.github.com>
2023-04-05 21:52:09 +0100

    feat: add makes requests test helper trait

---
+++ tests/MakesRequests.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Photogabble\Tuppence\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * @mixin TestCase
+ */
+trait MakesRequests
+{
+    protected function makeRequest(string $method = 'GET', ?array $params = null) {
+        $request = $this
+            ->getMockBuilder(ServerRequestInterface::class)
+            ->getMock();
+        $request->method('getMethod')->willReturn($method);
+        $request->method('getParsedBody')->willReturn($params);
+        return $request;
+    }
+
+    protected function makeRequestHandler()
+    {
+        $delegate = $this
+            ->getMockBuilder(RequestHandlerInterface::class)
+            ->getMock();
+        $delegate->method('handle')->willReturn($this->makeResponse());
+
+        return $delegate;
+    }
+
+    protected function makeResponse()
+    {
+        return $this
+            ->getMockBuilder(ResponseInterface::class)
+            ->getMock();
+    }
+}
