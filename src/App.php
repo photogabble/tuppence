d4cb01acd411d66306bc5d0b61d66ae676e082ef
Simon <carbontwelve@users.noreply.github.com>
2023-04-05 22:12:03 +0100

    feat: publish v2.0.3

--- src/App.php
+++ src/App.php
@@ -30,7 +30,7 @@ class App implements EventDispatcherAware
     /**
      * Tuppence Version.
      */
-    const VERSION = '2.0.2';
+    const VERSION = '2.0.3';
 
     use EventDispatcherAwareBehavior;
 
